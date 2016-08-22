1782ee1fdf53d2b6e81f1b2c04bda5be85808b80
Kenta Suzuki <k-suzuki@mi-labo.co.jp>
2016-08-22 11:21:19 +0900

    add todos api

--- django_rest_todo/api/views.py
+++ django_rest_todo/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Todo
+from .serializers import TodoSerializer
 
 # Create your views here.
+
+
+class TodoViewSet(ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
